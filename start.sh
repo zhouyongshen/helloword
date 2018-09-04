b3fe32d278f3699c79f5993f4a2296b5286d8cf3
zhouyongshen <954649679@qq.com>
2018-09-04 15:34:01 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#将jar移动到当前目录 避免重建时影响程序的运行
+echo '将jar移动到当前目录 避免重建时影响程序的运行'
 mv target/helloword-0.0.1-SNAPSHOT.jar ./
-#TODO 停止已启动的服务
-
-#启动服务 并后台挂起执行
+echo '停止已启动的服务'
+ps -ef|grep helloword-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs kill -9
+echo '启动服务 并后台挂起执行'
 nohup java -jar helloword-0.0.1-SNAPSHOT.jar >>log.log &
